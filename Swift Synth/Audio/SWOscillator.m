49945766a3c04ad6b2c81cffa3bb9eefa5eae0e0
Drago Ruzman <drago@ruzman.net>
2020-06-29 23:08:27 +0200

    Implementing all oscillators

--- Swift Synth/Audio/SWOscillator.m
+++ Swift Synth/Audio/SWOscillator.m
@@ -8,6 +8,8 @@
 
 #import "SWOscillator.h"
 
+#define RANDOM(smallNumber, bigNumber) ((((float) (arc4random() % ((unsigned)RAND_MAX + 1)) / RAND_MAX) * (bigNumber - smallNumber)) + smallNumber)
+
 float SWOscillatorAmplitude = 1.0;
 float SWOscillatorFrequency = 440.0;
 
@@ -21,25 +23,47 @@ float SWOscillatorFrequency = 440.0;
 
 +(SWSignalFunctionType) triangle {
     return ^float(float time) {
-        return SWOscillatorAmplitude * sin(2.0 * M_PI * SWOscillatorFrequency * time);
+        double period = 1.0 / (double)SWOscillatorFrequency;
+        double currentTime = fmod((double)time, period);
+
+        double value = currentTime / period;
+
+        double result = 0.0;
+
+        if (value < 0.25) {
+            result = value * 4.0;
+        }
+        else if (value < 0.75) {
+            result = 2.0 - (value * 4.0);
+        } else {
+            result = value * 4.0 - 4.0;
+        }
+
+        return SWOscillatorAmplitude * (float)result;
     };
 }
 
 +(SWSignalFunctionType) sawtooth {
     return ^float(float time) {
-        return SWOscillatorAmplitude * sin(2.0 * M_PI * SWOscillatorFrequency * time);
+        double period = 1.0 / (double)SWOscillatorFrequency;
+        double currentTime = fmod((double)time, period);
+
+        return SWOscillatorAmplitude * (((float)currentTime / period) * 2.0 - 1.0);
     };
 }
 
 +(SWSignalFunctionType) square {
     return ^float(float time) {
-        return SWOscillatorAmplitude * sin(2.0 * M_PI * SWOscillatorFrequency * time);
+        double period = 1.0 / (double)SWOscillatorFrequency;
+        double currentTime = fmod((double)time, period);
+
+        return ((currentTime / period) < 0.5) ? SWOscillatorAmplitude : -1.0 * SWOscillatorAmplitude;
     };
 }
 
 +(SWSignalFunctionType) whiteNoise {
     return ^float(float time) {
-        return SWOscillatorAmplitude * sin(2.0 * M_PI * SWOscillatorFrequency * time);
+        return SWOscillatorAmplitude * RANDOM(-1.0, 1.0);
     };
 }
 
